da7e9ff53a61ec5fb54ad78c6db258ea7604d7d2
Justin Pease <justinpease2@gmail.com>
2015-11-03 15:02:20 -0700

    finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,9 +9,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password:              "foo",
                                password_confirmation: "bar" }
     end
+    assert_select 'div.<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
     assert_template 'users/new'
   end
-  
+
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -21,6 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert is_logged_in?
   end
-
 end
